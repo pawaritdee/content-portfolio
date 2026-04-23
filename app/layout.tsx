69174075e0712d1e9197055f4a375c4e35196ca3
Dee <132458280+pawaritdee@users.noreply.github.com>
2026-04-23 20:13:09 +1000

    Edited  metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,24 +9,6 @@ const _geistMono = Geist_Mono({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: 'Dee Sethi | Digital Producer',
   description: 'I blend video production, community building, and technical expertise to help tech brands grow.',
-  generator: 'v0.app',
-  icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
-  },
 }
 
 export default function RootLayout({
